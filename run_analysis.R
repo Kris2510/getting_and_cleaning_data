271d3a684812d2f93c62f5951b4a75bb2f9fa1e5
Chris <Christofer.Henn@uni-hohenheim.com>
2015-07-26 18:36:29 +0200

    script

---
+++ run_analysis.R
@@ -0,0 +1,49 @@
+
+library(plyr)
+library(dplyr)
+
+d_features= read.table("./getting_and_cleaning/features.txt")
+d_train_x = read.table("./getting_and_cleaning/train/X_train.txt")
+names(d_train_x) <- d_features$V2
+d_test_x = read.table("./getting_and_cleaning/test/X_test.txt")
+names(d_test_x) <- d_features$V2
+joined <- rbind(d_test_x, d_train_x)
+
+
+d_train_y <-read.table("./getting_and_cleaning/train/Y_train.txt")
+d_test_y <-read.table("./getting_and_cleaning/test/Y_test.txt")
+
+y_joined <- rbind(d_test_y, d_train_y)
+colnames(y_joined) <- "Activity"
+
+d_train_sub <-read.table("./getting_and_cleaning/train/subject_train.txt")
+d_test_sub <-read.table("./getting_and_cleaning/test/subject_test.txt")
+
+sub_joined <- rbind(d_test_sub, d_train_sub)
+colnames(sub_joined) <- "Subject"
+
+
+joined$Activity <- y_joined$Activity
+joined$Subject <- sub_joined$Subject
+
+
+joined <- joined[ , !duplicated(colnames(joined))]
+
+joined_reduced <- select(joined, Activity, Subject, contains("mean()"), contains("std()"))
+
+
+
+
+joined_reduced$Activity[joined_reduced$Activity == "1"] <- "Walking"
+joined_reduced$Activity[joined_reduced$Activity == "2"] <- "Walking_Upstairs"
+joined_reduced$Activity[joined_reduced$Activity == "3"] <- "Walking_Downstairs"
+joined_reduced$Activity[joined_reduced$Activity == "4"] <- "Sitting"
+joined_reduced$Activity[joined_reduced$Activity == "5"] <- "Standing"
+joined_reduced$Activity[joined_reduced$Activity == "6"] <- "Laying"
+
+
+grouped <- joined_reduced %>% group_by(Subject,Activity)
+tidy_data_average <-grouped %>% summarise_each(funs(mean))
+
+write.table(tidy_data_average, file="tidy_data_average.txt", row.name=FALSE)
+
